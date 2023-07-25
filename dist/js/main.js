7dc26847cffabfa01f5d3082c32eb156de586a60
kolya2022 <fox20o1234@gmail.com>
2023-07-25 19:07:43 +0300

    push git hub

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+document.querySelector(".header__burger").addEventListener("click",(function(e){document.querySelector(".header__navbar").classList.toggle("active"),document.querySelector(".header__burger").classList.toggle("active")}));
